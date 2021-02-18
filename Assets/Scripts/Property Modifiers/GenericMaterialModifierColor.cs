adeb6b7ee5941f4edc91f80b427f5bf4543f5733
hendos43 <55980174+hendos43@users.noreply.github.com>
2021-02-18 21:58:36 +0000

    Made Scripts easier to understand in Inspector

--- Assets/Scripts/Property Modifiers/GenericMaterialModifierColor.cs
+++ Assets/Scripts/Property Modifiers/GenericMaterialModifierColor.cs
@@ -3,27 +3,23 @@ using UnityEngine;
 
 namespace Property_Modifiers
 {
-    public class GenericMaterialModifier : MonoBehaviour
+    public class GenericMaterialModifierColor : MonoBehaviour
     {
         [TextArea] public string notes;
         [Header("Object with Material to Modify")]
         public GameObject objectToModify;
         [Header("Property of Material to Modify")]
-        public string propertyToChange = "_DisplacementPower";
+        public string propertyToChange = "_MainColor";
         [Header("Values to Modify")]
         [Tooltip("If enabled, Initial Value will be ignored")]public bool lerpFromCurrent = false;
-        public float initialValue;
-        private float valueToChange;
-        public float targetValue;
+        public Color initialValue;
+        private Color valueToChange;
+        public Color targetValue;
         [Header("Duration (s)")]
         public float overDuration = 5;
-    
-
 
         private Renderer rend;
 
-    
-    
         private void Awake()
         {
             rend = objectToModify.GetComponent<Renderer>();
@@ -34,35 +30,33 @@ namespace Property_Modifiers
         {
             if (lerpFromCurrent == true)
             {
-                initialValue = rend.material.GetFloat(propertyToChange);
+                initialValue = rend.material.GetColor(propertyToChange);
                 //Debug.Log(initialValue);
             } 
         
             StartCoroutine(LerpFunction(targetValue, overDuration));
         }
 
-        IEnumerator LerpFunction(float endValue, float duration)
+        IEnumerator LerpFunction(Color endValue, float duration)
         {
             float time = 0;
             valueToChange = initialValue;
-            float startValue = initialValue;
+            Color startValue = initialValue;
 
             while (time < duration)
             {
-                valueToChange = Mathf.Lerp(startValue, endValue, time / duration);
+                valueToChange = Color.Lerp(startValue, endValue, time / duration);
                 time += Time.deltaTime;
                 yield return null;
             
                 //Set property
-                rend.material.SetFloat(propertyToChange, valueToChange);
+                rend.material.SetColor(propertyToChange, valueToChange);
             }
             valueToChange = endValue;
         
             //Set final value
-            rend.material.SetFloat(propertyToChange, valueToChange);
+            rend.material.SetColor(propertyToChange, valueToChange);
 
         }
     }
 }
-
-
--- Assets/Scripts/Property Modifiers/GenericMaterialModifierVector3.cs
+++ Assets/Scripts/Property Modifiers/GenericMaterialModifierVector3.cs
@@ -1,61 +1,62 @@
-using System;
 using System.Collections;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Runtime.CompilerServices;
-using TMPro;
-using Unity.Collections;
-using UnityEditor.ShaderGraph.Drawing;
 using UnityEngine;
 
-public class GenericMaterialModifierVector3 : MonoBehaviour
+namespace Property_Modifiers
 {
-    public GameObject objectToModify;
-    public bool lerpFromCurrent = false;
-    public Vector3 initialValue;
-    private Vector3 valueToChange;
-    public Vector3 targetValue;
-    public float overDuration = 5;
-    public string propertyToChange = "_MainColor";
-
-    private Renderer rend;
-
-    private void Awake()
+    public class GenericMaterialModifierVector3 : MonoBehaviour
     {
-        rend = objectToModify.GetComponent<Renderer>();
+        [TextArea] public string notes;
+        [Header("Object with Material to Modify")]
+        public GameObject objectToModify;
+        [Header("Property of Material to Modify")]
+        public string propertyToChange = "_MainColor";
+        [Header("Values to Modify")]
+        [Tooltip("If enabled, Initial Value will be ignored")]public bool lerpFromCurrent = false;
+        public Vector3 initialValue;
+        private Vector3 valueToChange;
+        public Vector3 targetValue;
+        [Header("Duration (s)")]
+        public float overDuration = 5;
 
-    }
+        private Renderer rend;
 
-    void OnEnable()
-    {
-        if (lerpFromCurrent == true)
+        private void Awake()
         {
-            initialValue = rend.material.GetVector(propertyToChange);
-            //Debug.Log(initialValue);
-        } 
-        
-        StartCoroutine(LerpFunction(targetValue, overDuration));
-    }
+            rend = objectToModify.GetComponent<Renderer>();
 
-    IEnumerator LerpFunction(Vector3 endValue, float duration)
-    {
-        float time = 0;
-        valueToChange = initialValue;
-        Vector3 startValue = initialValue;
-
-        while (time < duration)
-        {
-            valueToChange = Vector3.Lerp(startValue, endValue, time / duration);
-            time += Time.deltaTime;
-            yield return null;
-            
-            //Set property
-         rend.material.SetVector(propertyToChange, valueToChange);
         }
-        valueToChange = endValue;
-        
-        //Set final value
-        rend.material.SetVector(propertyToChange, valueToChange);
 
+        void OnEnable()
+        {
+            if (lerpFromCurrent == true)
+            {
+                initialValue = rend.material.GetVector(propertyToChange);
+                //Debug.Log(initialValue);
+            } 
+        
+            StartCoroutine(LerpFunction(targetValue, overDuration));
+        }
+
+        IEnumerator LerpFunction(Vector3 endValue, float duration)
+        {
+            float time = 0;
+            valueToChange = initialValue;
+            Vector3 startValue = initialValue;
+
+            while (time < duration)
+            {
+                valueToChange = Vector3.Lerp(startValue, endValue, time / duration);
+                time += Time.deltaTime;
+                yield return null;
+            
+                //Set property
+                rend.material.SetVector(propertyToChange, valueToChange);
+            }
+            valueToChange = endValue;
+        
+            //Set final value
+            rend.material.SetVector(propertyToChange, valueToChange);
+
+        }
     }
 }
