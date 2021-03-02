c569eaee5d1f968fe2b025c35e2e6dc80ad7b316
hendos43 <55980174+hendos43@users.noreply.github.com>
2021-03-02 18:30:11 +0000

    Added RotateY script for head & updated scene

---
+++ Assets/Scripts/RotateY.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateY : MonoBehaviour
+{
+    public float degreesPerSecond = 1; 
+
+    // Update is called once per frame
+    void Update()
+    {
+       transform.Rotate(Vector3.up * Time.deltaTime * degreesPerSecond);
+        
+    }
+}
